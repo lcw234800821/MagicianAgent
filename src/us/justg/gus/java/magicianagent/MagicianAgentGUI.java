9016ebc355a3524e5c5e3c5a25fd604631beefd2
gussmith23 <hfs5022@psu.edu>
2014-11-10 22:02:50 -0500

    Adds tabs to GUI.

--- src/us/justg/gus/java/magicianagent/MagicianAgentGUI.java
+++ src/us/justg/gus/java/magicianagent/MagicianAgentGUI.java
@@ -1,10 +1,15 @@
 
 package us.justg.gus.java.magicianagent;
 
+import com.sun.java.swing.plaf.windows.WindowsClassicLookAndFeel;
+import com.sun.java.swing.plaf.windows.WindowsLookAndFeel;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
 import javax.swing.JFrame;
 import javax.swing.JTabbedPane;
+import javax.swing.LookAndFeel;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
 
 /**
  *
@@ -17,26 +22,36 @@ import javax.swing.JTabbedPane;
 public class MagicianAgentGUI extends JFrame {
     
     // Dimension of the window. 
-    private final Dimension d = new Dimension(400,120);
+    private final Dimension d = new Dimension(400,400);
     
     JTabbedPane jTabbedPane;
     
     
     public MagicianAgentGUI() {
-        // Open window and set properties.
+        // Open window.
         super("Magician Agent");
+        
+        try{
+            UIManager.setLookAndFeel(new WindowsClassicLookAndFeel());
+        } catch(UnsupportedLookAndFeelException e){
+            
+        }
+        
+        // Set window properties.
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setSize(d);                                     // Set size
-        setResizable(false);                            // Freeze size
+        //setResizable(false);                            // Freeze size
         //setLayout(new FlowLayout(FlowLayout.CENTER));   // Layout
         setLocationRelativeTo(null);                    // Center window
         
         jTabbedPane = new JTabbedPane();
+        jTabbedPane.addTab("Status", null, new StatusTab(), "Check the status of a holiday or magician");
+        jTabbedPane.addTab("Book", null, new BookTab(), "Book a magician");
+        
+        
+        
+        // Finally, add components and set visible.
         add(jTabbedPane);
-        
-        
-        
-        // Finally, set visible.
         setVisible(true);
     }
 }
